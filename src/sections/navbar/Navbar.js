9331e45acf51e02cb5459bd680f173041965bad5
Jail_IB <jailsonsanches9@gmail.com>
2023-01-12 13:00:45 +0100

    feat:navbar

--- src/sections/navbar/Navbar.js
+++ src/sections/navbar/Navbar.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/jailson-logo.png'
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
+import { IoIosColorPalette } from 'react-icons/io'
+import data from './data'
 
 const Navbar = () => {
   return (
     <nav id='navbar'>
-      <div className="container"></div>
-    
+      <Box className="container nav__container" p={0} m={0}>
+            <a href="#" className='nav__logo'>
+              <img src={logo} alt="logo" id='nav-img' />
+            </a>      
+            <ul className='nav__menu'>
+              {data.map((item)=>{ return(
+                <li key={item.id}><a href={item.link}>{item.title}</a></li>
+              )
+                
+              })}
+            </ul>
+            <button className='theme__icon'> <IoIosColorPalette /> </button>
+      </Box>
     </nav>
   )
 }
